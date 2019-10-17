62d3eb4b5d1fd3673f79bd5467e6d4ce2f93b7c3
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-17 10:11:42 +0200

    displaying the skeleton with Bootstrap & jQuery in script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,64 @@
-var submitButtonEl = document.querySelector(".submitButton");
-var hourLabelEl = document.querySelector(".hourLabel");
-var taskTextEl = document.querySelector(".taskText");
+var submitButtonEl = document.querySelector(".svnBtn");
+var hourEl = document.querySelector(".hour");
+var newTask = document.querySelector(".form-control");
 var dispTaskEl = Array();
+var keyName = "hourSlot";
 
 //display current day
-document.querySelector("#currentDay").textContent = moment().format(
-  "MMM Do YY"
-);
+$("#currentDay").text(moment().format("MMM Do YY"));
 
 // displaying the whole page
+var i = 9;
+for (i = 9; i < 18; i++) {
+  // rowHour - every row
+  var rowHour = $("<div>");
+  rowHour.addClass("flex-row d-flex");
+  rowHour.attr("value-timeslot", i);
 
-// for saving in the local Storage, key = hour, value = "task text":
-var toDoTask = { hourLabelEl: taskTextEl };
+  //   append the row to the container
+  $(".container").append(rowHour);
 
-// on click submit a new element to the localStorage
-submitButtonEl.addEventListener("click", saveTask);
+  //   hourLabel - left column of the row with the time slot
+  var hourLabel = $("<div>");
+  hourLabel.addClass("col-form-label col-1");
+  hourLabel.text(i + ":00");
 
-// how to create a new element in the localStorage; we need to display the task in the text field, too...
-function saveTask(params) {
-  localStorage.setItem(hourLabelEl, taskTextEl);
+  //append the left col to the row
+  rowHour.append(hourLabel);
+
+  // middle column with the task text
+  var mCol = $("<div>");
+  mCol.addClass("col-10");
+  var taskText = $("<textarea>");
+  taskText.addClass("form-control bg-light");
+  taskText.attr("rows", 1);
+
+  //append the middle col to the row
+  rowHour.append(mCol);
+  mCol.append(taskText);
+
+  //right column - save button
+  var rCol = $("<div>");
+  rCol.addClass("col-1");
+  var saveButton = $("<button>");
+  saveButton.addClass("btn btn-success btn-block");
+  saveButton.attr("type", "submit");
+  saveButton.attr("type", "button");
+  saveButton.text("Save");
+
+  //append to the row
+  rowHour.append(rCol);
+  rCol.append(saveButton);
 }
+
+// for saving in the local Storage, key = hour slot, value = "task text":
+// var toDoTask = { hourEl: newTask.textContent };
+
+// on click submit a new element to the localStorage and update the text field
+// submitButtonEl.addEventListener("click", saveTask);
+
+//create a new element in the localStorage; display the task in the text field, too...
+function saveTask() {}
 
 // how to change the rows depending on the time - past, current, future
 function timeColors(params) {}
